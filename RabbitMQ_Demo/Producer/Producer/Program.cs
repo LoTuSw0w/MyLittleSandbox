ae95e8e15c361c3cc72c984a4fa51e2b74828a6d
Dung Luong <dung291196@gmail.com>
2022-12-09 00:48:19 +0100

    Added a sample Producer and Consumer

---
+++ RabbitMQ_Demo/Producer/Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var message = "HELLO THIS IS MY FIRST MESSAGE";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
