add900f641e398e1a07efc6a8d9be969dcd4c85b
Dung Luong <dung291196@gmail.com>
2023-07-03 23:49:11 +0200

    implemented a simple REPL

--- GoInterpreter/main.go
+++ GoInterpreter/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"LotusProgrammingLanguage/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
 
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s, and welcome to the Lotus Programming Language\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
 }
